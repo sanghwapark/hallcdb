5783ac2a29068ee0f9eaa3830d0b211ec117ff94
sanghwapark <sanghwapark@gmail.com>
2025-07-15 10:00:21 -0400

    update setup script to use rcdb python library

--- setup.csh
+++ setup.csh
@@ -1,31 +1,31 @@
 #!/bin/csh
 
-set RCDB_DIR=/home/cdaq/rcdb/rcdb
+#set RCDB_DIR=/home/cdaq/rcdb/rcdb
 set HCDB_DIR=/home/cdaq/rcdb/hallcdb
 
 # RCDB environment 
-if ( ! $?RCDB_HOME ) then
-    setenv RCDB_HOME $RCDB_DIR
-endif
-
-if ( ! $?LD_LIBRARY_PATH ) then
-    setenv LD_LIBRARY_PATH $RCDB_HOME/cpp/lib
-else
-    setenv LD_LIBRARY_PATH $RCDB_HOME/cpp/lib:$LD_LIBRARY_PATH
-endif
-
-if ( ! $?PYTHONPATH ) then
-    setenv PYTHONPATH $RCDB_HOME/python
-else
-    setenv PYTHONPATH $RCDB_HOME/python:$PYTHONPATH
-endif
-
-setenv PATH "$RCDB_HOME":"$RCDB_HOME/bin":"$RCDB_HOME/cpp/bin":$PATH
+#if ( ! $?RCDB_HOME ) then
+#    setenv RCDB_HOME $RCDB_DIR
+#endif
+#
+#if ( ! $?LD_LIBRARY_PATH ) then
+#    setenv LD_LIBRARY_PATH $RCDB_HOME/cpp/lib
+#else
+#    setenv LD_LIBRARY_PATH $RCDB_HOME/cpp/lib:$LD_LIBRARY_PATH
+#endif
+#
+#if ( ! $?PYTHONPATH ) then
+#    setenv PYTHONPATH $RCDB_HOME/python
+#else
+#    setenv PYTHONPATH $RCDB_HOME/python:$PYTHONPATH
+#endif
+#
+#setenv PATH "$RCDB_HOME":"$RCDB_HOME/bin":"$RCDB_HOME/cpp/bin":$PATH
 
 # Hall C
 setenv PYTHONPATH "$HCDB_DIR":$PYTHONPATH
 
 
 # Connection string
-setenv RCDB_CONNECTION mysql://rcdb@hallcdb.jlab.org/c-rcdb
+setenv RCDB_CONNECTION mysql://rcdb@hcdb1.jlab.org/rsidis
 #setenv RCDB_CONNECTION mysql://rcdb@cdaqdb1.jlab.org/rcdb
--- setup.sh
+++ setup.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-RCDB_DIR=/home/cdaq/rcdb/rcdb
+#SP: Commented out, Use RCDB Python library
+#RCDB_DIR=/home/cdaq/rcdb/rcdb
+
 HCDB_DIR=/home/cdaq/rcdb/hallcdb
 
 # RCDB environment 
-if [[ -z "$RCDB_HOME" ]]; then
-    export RCDB_HOME=$RCDB_DIR
-fi
-
-if [[ -z "$LD_LIBRARY_PATH" ]]; then
-    export LD_LIBRARY_PATH=$RCDB_HOME/cpp/lib
-else
-    export LD_LIBRARY_PATH=$RCDB_HOME/cpp/lib:$LD_LIBRARY_PATH
-fi
-
-if [[ -z "$PYTHONPATH" ]]; then
-    export PYTHONPATH=$RCDB_HOME/python
-else
-    export PYTHONPATH=$RCDB_HOME/python:$PYTHONPATH
-fi
-
-export PATH="$RCDB_HOME":"$RCDB_HOME/bin":"$RCDB_HOME/cpp/bin":$PATH
+#if [[ -z "$RCDB_HOME" ]]; then
+#    export RCDB_HOME=$RCDB_DIR
+#fi
+#
+#if [[ -z "$LD_LIBRARY_PATH" ]]; then
+#    export LD_LIBRARY_PATH=$RCDB_HOME/cpp/lib
+#else
+#    export LD_LIBRARY_PATH=$RCDB_HOME/cpp/lib:$LD_LIBRARY_PATH
+#fi
+#
+#if [[ -z "$PYTHONPATH" ]]; then
+#    export PYTHONPATH=$RCDB_HOME/python
+#else
+#    export PYTHONPATH=$RCDB_HOME/python:$PYTHONPATH
+#fi
+#
+#export PATH="$RCDB_HOME":"$RCDB_HOME/bin":"$RCDB_HOME/cpp/bin":$PATH
 
 # Hall C
 export PYTHONPATH="$HCDB_DIR":$PYTHONPATH
 
 
 # Connection string
-export RCDB_CONNECTION=mysql://rcdb@cdaqdb1.jlab.org/c-rcdb
+export RCDB_CONNECTION=mysql://rcdb@hcdb1.jlab.org/rsidis
 #export RCDB_CONNECTION=mysql://rcdb@cdaqdb1.jlab.org/rcdb
